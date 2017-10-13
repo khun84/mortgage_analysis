2aa82a1de6ee3f29e77bb8b28248af9df24f0a89
khun84 <khun84@gmail.com>
2017-10-13 09:28:29 +0800

    created table for users and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # FACEBOOK AUTHENTICATION
+    get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+
+    resources :users
+
+
 end
